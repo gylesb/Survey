091c4a01945d0786e44e1233d749684f645f15dd
Gyles Batara and Marcus Parmentier <gyles@>
2017-09-13 14:17:34 -0700

    Included further js

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,22 @@
 $(document).ready(function() {
 
-  $("#formInput").submit(function(event) {
-    var name = $("input#name").val();
-    var food = $("#food").val();
-    var music = $("input:radio[name=music]:checked").val();
-    var sports = $("input#sports").val();
-    var born = $("#born").val();
-    var color = $("#color").val();
+  $("#btnSubmit").click(function() {
+
+    $("#FormOne").submit(function(event) {
+      var name = $("input#name").val();
+      var food = $("#food").val();
+      var music = $("input:radio[name=music]:checked").val();
+      var sports = $("input#sports").val();
+      var born = $("#born").val();
+      var color = $("#color").val();
+      alert(name);
 
 
+      $("#name").text(nameInput);
 
+      $("#thankyou").show();
 
-
-    event.preventDefault();
+      event.preventDefault();
+    });
   });
 });
